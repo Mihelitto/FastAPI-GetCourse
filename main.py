476294ec3bbccd040f20d8cb673c6f4d1050fb0f
Michael Yudin <mihelitto@gmail.com>
2021-08-17 23:39:07 +0300

    Add base endpoints

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Deal(BaseModel):
+    deal_number: int
+    offer_code: str
+    product_title: str
+    deal_cost: int
+    user_email: str
+
+
+@app.get("/{user_name}/deals")
+def get_deals(user_name: str, status: Optional[str] = 'new'):
+    return {"Name": user_name, "status": status}
+
+
+@app.post("/{user_name}/deals")
+def post_deals(deal:Deal):
+    return deal
