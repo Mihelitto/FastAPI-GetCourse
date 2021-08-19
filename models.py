d09528c557a1492a6800399e8de7c81e33c2d233
Michael Yudin <mihelitto@gmail.com>
2021-08-19 22:41:21 +0300

    Add basic authorization

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -7,3 +8,14 @@ class Deal(BaseModel):
     product_title: str
     deal_cost: int
     user_email: str
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
