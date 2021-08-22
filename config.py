7797cbb13bb227c471b8c7c9bb61169e8117ef72
Michael Yudin <mihelitto@gmail.com>
2021-08-23 01:51:43 +0300

    Add start by docker-compose

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ env.read_env()
 
 secret_key = env.str('SECRET_KEY', 'REPLACE_ME')
 secret_token_key = env.str('SECRET_TOKEN_KEY', 'REPLACE_ME')
-database = env.str('DATABASE', 'fastApi')
+database = env.str('DATABASE_NAME', 'fastApi')
 db_user = env.str('DB_USER')
 db_password = env.str('DB_PASSWORD')
 db_host = env.str('DB_HOST')
